fdf88f8ee9076fe3217093361e665f2e892061cd
Martin Lensment <mlensment@gmail.com>
2012-07-05 00:53:58 +0300

    Normalize tilt debugging

--- js/ball.js
+++ js/ball.js
@@ -40,6 +40,6 @@ Ball.prototype.draw = function(ctx) {
 
 Ball.prototype.debug = function() {
   $('#debug').html('Acceleration vector: (' + this.acceleration.x.toFixed(2) + ', ' + this.acceleration.y.toFixed(2) + ')<br/>\
-    Velocity vector: (' + Math.round(this.velocity.x) + ', ' + Math.round(this.velocity.y) + ')\
-    Tilt: ' + this.tiltDebug['x'] + ', ' + this.tiltDebug['y']);
+    Velocity vector: (' + Math.round(this.velocity.x) + ', ' + Math.round(this.velocity.y) + ')<br/>\
+    Tilt: (' + this.tiltDebug['x'].toFixed(2) + ', ' + this.tiltDebug['y'].toFixed(2) + ')');
 };

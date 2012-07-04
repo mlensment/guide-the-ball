6826384bc09cbcc136aed61e1b8f72dceee209e5
Martin Lensment <mlensment@gmail.com>
2012-07-04 15:39:30 +0300

    Cleaner debugging

--- js/ball.js
+++ js/ball.js
@@ -12,19 +12,15 @@ var Ball = function() {
 };
 
 Ball.prototype.update = function() {
-  //for debugging
-  var c = document.getElementById('guide-the-ball');
-  var ctx = c.getContext('2d');
-  c.width = c.width;
-
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.font = '10px Calibri';
-  ctx.fillText(this.orientation['alpha'], c.width / 2, (c.height / 2.3) + 20);
-  ctx.fillText(this.orientation['beta'], c.width / 2, (c.height / 2.3) + 40);
-  ctx.fillText(this.orientation['gamma'], c.width / 2, (c.height / 2.3) + 60);
+  this.debug();
 };
 
 Ball.prototype.draw = function() {
 
+};
+
+Ball.prototype.debug = function() {
+  $('#debug').html('Alpha: ' + this.orientation['alpha'] + '<br>\
+    Beta: ' + this.orientation['beta'] + '<br>\
+    Gamma: ' + this.orientation['gamma']);
 };

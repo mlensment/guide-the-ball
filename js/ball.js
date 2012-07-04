053f3f7850884c319019087c2e04f25d3d5f771a
Martin Lensment <mlensment@gmail.com>
2012-07-04 15:33:46 +0300

    This to self

--- js/ball.js
+++ js/ball.js
@@ -3,11 +3,11 @@ var Ball = function() {
   this.height = 10;
   this.position = [0, 0];
   this.orientation = { 'alpha': 0, 'beta': 0, 'gamma': 0 };
-
+  var self = this;
   window.addEventListener('deviceorientation', function(event) {
-    this.orientation['alpha'] = event.alpha;
-    this.orientation['beta'] = event.beta;
-    this.orientation['gamma'] = event.gamma;
+    self.orientation['alpha'] = event.alpha;
+    self.orientation['beta'] = event.beta;
+    self.orientation['gamma'] = event.gamma;
   }, false);
 };
 
@@ -16,7 +16,7 @@ Ball.prototype.update = function() {
   var c = document.getElementById('guide-the-ball');
   var ctx = c.getContext('2d');
   c.width = c.width;
-  
+
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
   ctx.font = '10px Calibri';

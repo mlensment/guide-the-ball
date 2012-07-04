1debeaa49d26529e4c7dcb9aa5e6788c1779258c
Martin Lensment <mlensment@gmail.com>
2012-07-04 23:07:47 +0300

    Switch vector positions

--- js/ball.js
+++ js/ball.js
@@ -11,8 +11,8 @@ var Ball = function() {
     self.orientation['alpha'] = Math.round(event.alpha);
     self.orientation['beta'] = Math.round(event.beta);
     self.orientation['gamma'] = Math.round(event.gamma);
-    self.acceleration.x = Math.round(event.beta) / 100;
-    self.acceleration.y = Math.round(event.gamma) / 100;
+    self.acceleration.x = Math.round(event.gamma) / 100;
+    self.acceleration.y = Math.round(event.beta) / 100;
   }, false);
 };
 
